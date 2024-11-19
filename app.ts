46d34e1401c3716e505e134077a3a653d21bad2f
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2024-11-20 00:29:28 +0100

    chore: disable oauth2 debug flag

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import { TadoOAuth2Client } from "./lib/tado-oauth2-client";
 
 module.exports = class TadoHomeApp extends OAuth2App<TadoOAuth2Client> {
     static override OAUTH2_CLIENT = TadoOAuth2Client;
-    static override OAUTH2_DEBUG = true;
+    static override OAUTH2_DEBUG = false;
     static override OAUTH2_MULTI_SESSION = false;
 
     override async onOAuth2Init(): Promise<void> {
